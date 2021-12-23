eeeeff1a8abeac72369156f8af8814f03d69bfc3
carloscr99 <carloscolomer99@gmail.com>
2021-12-23 17:57:39 +0100

    creación del proyecto Journal App, instalación de bootstrap y implementar SASS

---
+++ 07-journal/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+
+    <h1>Hola mundo</h1>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Seconday</button>
+    <button class="btn btn-success">Success</button>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
