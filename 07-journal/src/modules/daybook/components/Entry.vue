662f78ebeee3b4283b39763e2e70502edefc68b3
carloscr99 <carloscolomer99@gmail.com>
2022-01-04 18:04:34 +0100

    Creación de varios modulos

---
+++ 07-journal/src/modules/daybook/components/Entry.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="entry-container mb-3 pointer p-2" @click="$router.push({ name: 'entry', params: { id: 10} })">
+
+        <!-- Titulo -->
+        <div class="entry-title d-flex">
+            <span class="text-success fs-5 fw-bold"> 4 </span>
+            <span class="mx-1 fs-5">enero</span>
+            <span class="mx-2 fw-light">2022</span>
+        </div>
+
+        <div class="entry-description">
+            Laborum elit ea ad ipsum aliquip officia sint nisi pariatur et ut esse veniam. Ut occaecat in qui mollit. Officia adipisicing proident reprehenderit fugiat. Dolor officia officia consequat nulla est et nisi enim laborum irure. Exercitation ipsum minim Lorem laboris duis laborum quis eiusmod velit dolore voluptate.
+        </div>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.entry-container{
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover{
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description{
+        font-size: 12px;
+    }
+
+}
+
+
+</style>
--- 07-journal/src/modules/daybook/layouts/DayBookLayout.vue
+++ 07-journal/src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1>DayBook Layout</h1>
+    <div>
+
+        <Navbar/>
+        <div class="d-flex">
+        <div class="col-4">
+            <EntryList />
+        </div>
+        <div class="col">
+            <router-view />
+        </div>
+
+
+    </div>
+
+    </div>
+
 </template>
+
+<script>
+
+import { defineAsyncComponent } from 'vue'
+
+
+
+export default {
+    components:{
+        Navbar: defineAsyncComponent(()=> import('../components/Navbar.vue')),
+        EntryList: defineAsyncComponent(()=> import('../components/EntryList.vue')), 
+    }
+}
+</script>
