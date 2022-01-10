e59261ea90b8ca9e64d31ec55d866042dcd2d5c7
carloscr99 <carloscolomer99@gmail.com>
2022-01-10 17:42:03 +0100

    Introducción a VueX con modulos

---
+++ 07-journal/src/modules/daybook/store/journal/state.js
@@ -0,0 +1,21 @@
+
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+        id: new Date().getTime(),
+        date: new Date().toDateString(),
+        text: 'Et mollit ullamco sit aliqua dolore incididunt eu do deserunt laborum fugiat. Duis laborum dolore nisi officia. Tempor culpa sunt et Lorem velit amet non magna non proident quis velit. Culpa adipisicing laborum enim excepteur nostrud in elit labore cillum excepteur enim cupidatat sit. Do nisi adipisicing ea veniam id ex sint id cillum.',
+        picture: null,
+        
+    },
+    {
+        id: new Date().getTime() + 1000,
+        date: new Date().toDateString(),
+        text: 'Et mollit ullamco sit aliqua dolore incididunt eu do deserunt laborum fugiat. Duis laborum dolore nisi officia. Tempor culpa sunt et Lorem velit amet non magna non proident quis velit. Culpa adipisicing laborum enim excepteur nostrud in elit labore cillum excepteur enim cupidatat sit. Do nisi adipisicing ea veniam id ex sint id cillum.',
+        picture: null,
+        
+    },
+]
+})
